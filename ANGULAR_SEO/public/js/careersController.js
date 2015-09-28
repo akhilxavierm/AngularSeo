69dbd987aa3c4615a9c9070bd31f893310665639
Akhil Xavier <akhil@grabhoue.com>
2015-09-28 16:48:54 +0530

    Implimented redis for cachig purpose
    Implimented cached prerendred page return using prerender.io and redis

--- ANGULAR_SEO/public/js/careersController.js
+++ ANGULAR_SEO/public/js/careersController.js
@@ -1,4 +1,4 @@
 myModule.controller('careersController',function($scope,dynamicMetaFactory){
     dynamicMetaFactory.appId=20;
-    dynamicMetaFactory.appArgument="this is my custome headline for about page";
+    dynamicMetaFactory.appArgument="this is my custome headline for Careers page";
 });
--- ANGULAR_SEO/server.js
+++ ANGULAR_SEO/server.js
@@ -1,6 +1,8 @@
 //comment added in testing branch
 var express = require('express');
 var path = require('path');
+var redis = require("redis");
+var client = redis.createClient();
 
 var app = express();
 var http = require('http').Server(app);
@@ -10,7 +12,11 @@ app.set('view engine', 'ejs');
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(require('prerender-node').set('prerenderServiceUrl', 'http://localhost:3000/'));
+app.use(require('prerender-node').set('prerenderServiceUrl', 'http://localhost:3000/').set('beforeRender', function(req, done) {
+    client.get(req.url, done);
+}).set('afterRender', function(err, req, prerender_res) {
+    client.set(req.url, prerender_res.body)
+}));
 
 app.get('*', function (req, res) {
   
